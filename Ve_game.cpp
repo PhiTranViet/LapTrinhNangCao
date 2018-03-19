d608624b987e2a46232b970933ccb120ea641f0e
PhiTranViet <37354673+PhiTranViet@users.noreply.github.com>
2018-03-19 16:09:58 +0700

    Add files via upload

---
+++ Ve_game.cpp
@@ -0,0 +1,36 @@
+void game()
+{
+	gotoxy(3, 2);
+	textcolor(1+rand()%15);
+	cout<<"        GGGGGGGGGGGGG               AAA               MMMMMMMM               MMMMMMMMEEEEEEEEEEEEEEEEEEEEEE";
+	gotoxy(3, 3);
+	cout<<"     GGG::::::::::::G              A:::A              M:::::::M             M:::::::ME::::::::::::::::::::E";
+	gotoxy(3, 4);
+	cout<<"   GG:::::::::::::::G             A:::::A             M::::::::M           M::::::::ME::::::::::::::::::::E";
+	gotoxy(3, 5);
+	cout<<"  G:::::GGGGGGGG::::G            A:::::::A            M:::::::::M         M:::::::::MEE::::::EEEEEEEEE::::E";
+	gotoxy(3, 6);
+	cout<<" G:::::G       GGGGGG           A:::::::::A           M::::::::::M       M::::::::::M  E:::::E       EEEEEE";
+	gotoxy(3, 7);
+	cout<<"G:::::G                        A:::::A:::::A          M:::::::::::M     M:::::::::::M  E:::::E             ";
+	gotoxy(3, 8);
+	cout<<"G:::::G                       A:::::A A:::::A         M:::::::M::::M   M::::M:::::::M  E::::::EEEEEEEEEE   ";
+	gotoxy(3, 9);
+	cout<<"G:::::G    GGGGGGGGGG        A:::::A   A:::::A        M::::::M M::::M M::::M M::::::M  E:::::::::::::::E   ";
+	gotoxy(3, 10);
+	cout<<"G:::::G    G::::::::G       A:::::A     A:::::A       M::::::M  M::::M::::M  M::::::M  E:::::::::::::::E   ";
+	gotoxy(3, 11);
+	cout<<"G:::::G    GGGGG::::G      A:::::AAAAAAAAA:::::A      M::::::M   M:::::::M   M::::::M  E::::::EEEEEEEEEE   ";
+	gotoxy(3, 12);
+	cout<<"G:::::G        G::::G     A:::::::::::::::::::::A     M::::::M    M:::::M    M::::::M  E:::::E             ";
+	gotoxy(3, 13);
+	cout<<" G:::::G       G::::G    A:::::AAAAAAAAAAAAA:::::A    M::::::M     MMMMM     M::::::M  E:::::E       EEEEEE";
+	gotoxy(3, 14);
+	cout<<"  G:::::GGGGGGGG::::G   A:::::A             A:::::A   M::::::M               M::::::ME:::::: EEEEEEEEE::::E";
+	gotoxy(3, 15);
+	cout<<"   GG:::::::::::::::G  A:::::A               A:::::A  M::::::M               M::::::ME::::::::::::::::::::E";
+	gotoxy(3, 16);
+	cout<<"     GGG::::::GGG:::G A:::::A                 A:::::A M::::::M               M::::::ME::::::::::::::::::::E";
+	gotoxy(3, 17);
+	cout<<"        GGGGGG   GGGGAAAAAAA                   AAAAAAAMMMMMMMM               MMMMMMMMEEEEEEEEEEEEEEEEEEEEEE";
+}
